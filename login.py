c2c2c13c45e1fa9f84f6ed39adbd10fa3d7abebe
SrLucca <luccatsoares@gmail.com>
2020-10-20 11:35:08 -0300

    login

--- login.py
+++ login.py
@@ -3,18 +3,15 @@ from telausuario import screenuser
 
 def singin():
     while(True):
+        user_login = input('Insira seu CPF COMPLETO: ')
         try:
-            user_login = input('Insira seu CPF COMPLETO: ')
-            name_user = input('Insira seu NOME: ')
-            try:
-                past_archivo = os.mkdir('{}'.format(user_login))
-                print('USUARIO NÃO CADASTRADO')
-                os.removedirs((r'C:\Users\Lucia\Desktop\Lucca\programação\{}'.format(user_login)))
-                break
-            except FileExistsError:
-                print(f'Olá, {name_user}!')
-                screenuser()
-                break
-        except:
-            continue
+            past_archivo = os.mkdir('{}'.format(user_login))
+            print('USUARIO NÃO CADASTRADO')
+            os.removedirs((r'C:\Users\Lucia\Desktop\Lucca\programação\{}'.format(user_login)))
+            break
+        except FileExistsError:
+            os.chdir(r'C:\Users\Lucia\Desktop\Lucca\programação\{}'.format(user_login))
+            print(f'Olá,{os.listdir()}')
+            screenuser()
+            break
     return user_login
